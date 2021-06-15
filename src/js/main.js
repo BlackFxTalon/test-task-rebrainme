96adc2167d4b9cc51a15563a1ee09b53fc48dbdd
blackfxtalon <andyv@inbox.ru>
2021-06-15 11:03:05 +0500

    add no-scroll class to body

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,13 @@
 import "./import/blocks";
 import "./import/components";
 
+const modalBtn = document.querySelectorAll('.modal-btn');
+const body = document.querySelector('body');
 const authBtn = document.querySelector('.auth-btn');
 const modalOverlay = document.querySelector('.modal__overlay');
 const modalClose = document.querySelector('.modal__close');
 const programItem = document.querySelectorAll('.program__item');
+
 
 authBtn.addEventListener('click', () => {
     authBtn.classList.add('auth-btn--opened');
@@ -15,12 +18,19 @@ modalOverlay.addEventListener('click', () => {
     if (authBtn.classList.contains('auth-btn--opened')) {
         authBtn.classList.remove('auth-btn--opened');
     }
+    body.classList.remove('no-scroll');
 })
 
 modalClose.addEventListener('click', () => {
     if (authBtn.classList.contains('auth-btn--opened')) {
         authBtn.classList.remove('auth-btn--opened');
     }
+    body.classList.remove('no-scroll');
+})
+modalBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        body.classList.add('no-scroll');
+    })
 })
 
 programItem.forEach((item) => {
@@ -34,3 +44,4 @@ programItem.forEach((item) => {
         }
     })
 })
+
